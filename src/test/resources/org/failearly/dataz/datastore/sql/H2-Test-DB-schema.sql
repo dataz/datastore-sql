d9cdd37d5f702b7e5991e7ce73035385e2a9126c
Marko Umek <umek.marko@gmail.com>
2020-03-27 17:21:02 +0100

    Upgrade License to EPLv2.0

--- src/test/resources/org/failearly/dataz/datastore/sql/H2-Test-DB-schema.sql
+++ src/test/resources/org/failearly/dataz/datastore/sql/H2-Test-DB-schema.sql
@@ -1,3 +1,15 @@
+/*
+ * dataZ - Test Support For Data Stores.
+ *
+ * Copyright 2014-2020 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
 CREATE TABLE PUBLIC.USERS (
   ID   INT PRIMARY KEY NOT NULL,
   NAME VARCHAR2(256) NOT NULL
